b6a5edd78389f34134c67a86bc530390307e5738
Rajat Sharma <rajatwithphy@gmail.com>
2021-04-02 16:48:58 +0530

    modification in sandwichOrders program

--- sandwichOrders.py
+++ sandwichOrders.py
@@ -7,7 +7,7 @@ sandwichOrders = []
 finishedOrders = []
 
 i = 1
-print("Enter the names of sandwiches (type '\end' if you have made your list):")
+print("Enter the names of sandwiches (type '\end' if you have made your list):\n[Note that 'Deli' has run out of 'Pastrami']")
 while oneMore:
     toPrepare = input('{}{} '.format(i,'.'))
     if toPrepare == '\end':
@@ -20,15 +20,31 @@ i = 1
 
 print("\n----List of sandwhiches has been recorded.----\n")
 
-while sandwichOrders:
-    preparing = sandwichOrders.pop()
-    print("\nPreparing your",preparing.title(),"sandwich.")
+for item in sandwichOrders:
+    preparing = item.lower()
+    if preparing == 'deli':
+        print("\nNote that 'Deli' has run out of 'Pastrami'")
+        #sandwichOrders.remove('deli')
+    else:
+        print("\nPreparing your",preparing.title(),"sandwich.")
+        finishedOrders.append(preparing)
 
-    finishedOrders.append(preparing)
+    
 
 print("\nAll your sandwiches have been prepared.\nHere is the list.")
 
 for item in finishedOrders:
 
+    print(i,".","",item.title())
+    i += 1
+
+print("\nPrinting sandwich orders list:")
+i = 1
+
+'''for item in sandwichOrders:
+    if item == 'deli':
+        sandwichOrders.remove('deli')'''
+
+for item in sandwichOrders:
     print(i,".","",item.title())
     i += 1
