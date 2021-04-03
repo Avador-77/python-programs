14184dbbfd14694aeb61c3fe1661d5f3750fbcf9
Rajat Sharma <rajatwithphy@gmail.com>
2021-04-03 21:06:22 +0530

    adding projects

---
+++ Basics and Apps/ContactsBook.py
@@ -0,0 +1,33 @@
+# function for creating menu
+
+# function for getiing input
+
+def mainMenu():
+    print("A. Display Stored Contacts.")
+    print("B. Save a New Contact.")
+    print("C. Search a Contact.")
+    print("D. Quit.")
+
+def DisplayStoredContacts():
+
+
+def SaveNewConatact():
+
+def SearchContact():
+
+
+
+
+
+
+
+def gettingUserInput():
+    print("\t\t\t----Welcome to Avador's Advance Phone Book----\n")
+    mainMenu()
+    option = input("\nWhat do you want to do?(Choose from above options): ")
+    option = option.lower()
+    if option == 'a':
+        
+
+gettingUserInput()
+
---
+++ Basics and Apps/test.py
@@ -0,0 +1,3 @@
+num1, num2 = input("enter number 1: "), input("enter number 2:")
+print(num1)
+print(num2)
