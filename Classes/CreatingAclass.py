48b6f7b2613b2276fd4f618c31812ebde5b4b84f
Rajat Sharma <rajatwithphy@gmail.com>
2021-04-11 19:24:16 +0530

    creating a child class

--- Classes/CreatingAclass.py
+++ Classes/CreatingAclass.py
@@ -7,7 +7,7 @@ class Restaurant():
 
     def describe_restaurant(self):
         print("Restaurant Name: " + self.resName.title() + "\nCuisine Type: " + self.cuisineType.title() + "\nCustomers Served: " + str(self.number_served))
-        print("\nLet's update the number served using a method.")
+        
 
     def set_number_served(self, newCustomers):
         self.number_served = newCustomers
@@ -18,13 +18,29 @@ class Restaurant():
     def open_or_close(self):
         print("(" + self.resName.title() + " restaurant is Opened" + ")")
 
+class IceCreamStand(Restaurant):
 
+    def __init__(self):
+        
+        self.flavours = ['choclate', 'vanilla', 'butterscotch']
+
+    def display_flavours(self):
+        print("let's see what flavours do we have in our favourite ice cream parlour.")
+        print("Flavours = ",self.flavours)
+        
+        
+
+
+iceCream = IceCreamStand()
+#iceCream.describe_restaurant()
+iceCream.display_flavours()
 
 restaurant = Restaurant('the moon light', 'chinese')
 
 restaurant.open_or_close()
 restaurant.number_served = 25
 restaurant.describe_restaurant()
+print("\nLet's update the number served using a method.")
 restaurant.set_number_served(50)
 restaurant.increment_number_served(150)
 restaurant.describe_restaurant()
