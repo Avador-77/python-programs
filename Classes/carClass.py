9c3c8bf1e947ad8996f5f1d8a015b927691c0499
Rajat Sharma <rajatwithphy@gmail.com>
2021-04-11 17:44:57 +0530

    CarClass modifications

--- Classes/carClass.py
+++ Classes/carClass.py
@@ -16,11 +16,53 @@ class Car():
         else:
             print("You can't roll back an odometer.")
     
+    def fill_gas_tank(self):
+        print("This car has 30L gas tank.")
+
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
     def read_odometer(self):
         print("This car has ", self.odometer_reading, " miles on it.")
+
+class ElectricCar(Car):
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        #self.battery_size = 70   ----- providing value using a separate battery class   
+        self.battery = Battery()
+
+    """def describe_battery(self):     #shifting the method in battery class
+        print("This car has " + str(self.battery_size) + "-kWh battery.")"""
+    
+    def fill_gas_tank(self):
+        print("Tesla is an Electric Car so It has no gas tank.")
+
+class Battery():
+
+    def __init__(self, battery_size = 70):
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        print("This car has " + str(self.battery_size) + "-kWh battery.")
+    
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 80:
+            range = 270
+        message = "This car can travel " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+my_tesla = ElectricCar('Tesla', 'Model S', 2016)
+print(my_tesla.get_descriptive_name())
+
+my_tesla.battery.describe_battery()
+
+my_tesla.fill_gas_tank()
+
+my_tesla.battery.get_range()
 
 my_car = Car('audi', 'a4', 2016)
 
@@ -35,3 +77,5 @@ my_car.read_odometer()
 my_car.increment_odometer(12560)
 
 my_car.read_odometer()
+
+my_car.fill_gas_tank()
