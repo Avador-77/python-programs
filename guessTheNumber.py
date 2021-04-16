2a28ff828f21dd44cbf14bac64776c6d30caa844
Rajat Sharma <rajatwithphy@gmail.com>
2021-04-17 02:51:18 +0530

    learned how to indent right and left

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -2,42 +2,36 @@ import random
 
 def guess(x):
     random_number = random.randrange(x)
-    chance  = 1
+    chance  = 0
     guess = int(input(f"Guess a number between 1 to {x}:"))
 
 
-    active = True
-    while active:
-        while guess != random_number:
-            chance += 1
-            if chance != 5:
-                if guess < random_number:
+    while guess != random_number:
+        chance += 1
+        if chance != 5:
+            if guess < random_number:
 
-                
-                    
-
-                    if chance == 4:
-                        print("nope, that's not correct. Too low.")
-                        guess = int(input(f"----This is the last chance----Guess a number between 1 to {x} again:"))
-                    else:
-                        print("nope, that's not correct. Too low.")
-                        guess = int(input(f"Guess a number between 1 to {x} again:"))
-                    
+                if chance == 4:
+                    print("nope, that's not correct. Too low.")
+                    guess = int(input(f"----This is the last chance----Guess a number between 1 to {x} again:"))
                 else:
-
-                    if chance == 4:
-                        print("nope, that's not correct. Too high.")
-                        guess = int(input(f"----This is the last chance----Guess a number between 1 to {x} again:"))
-                    else:
-                        print("nope, that's not correct. Too high.")
-                        guess = int(input(f"Guess a number between 1 to {x} again:"))
-            
+                    print("nope, that's not correct. Too low.")
+                    guess = int(input(f"Guess a number between 1 to {x} again:"))
+                
             else:
-                print("You Lost!!")
-                break
-        if guess == random_number:
-            print(f"yeah, congratualtions. You guessed it, the number was {guess} ")
-        active = False
+
+                if chance == 4:
+                    print("nope, that's not correct. Too high.")
+                    guess = int(input(f"----This is the last chance----Guess a number between 1 to {x} again:"))
+                else:
+                    print("nope, that's not correct. Too high.")
+                    guess = int(input(f"Guess a number between 1 to {x} again:"))
+        
+        else:
+            print("You Lost!!")
+            break
+    if guess == random_number:
+        print(f"yeah, congratualtions. You guessed it, the number was {guess} ")
 
 rangee = int(input("Enter the range: "))
 guess(rangee)
